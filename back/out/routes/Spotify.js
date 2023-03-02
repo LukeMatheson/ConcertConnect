9cc1b6a4e752655bff0989b6861617d919f076ac
Nizom Djuraev <nd596@drexel.edu>
2023-03-01 21:26:40 -0500

    Implement server build and start for the project
    Implement server build and start for React project. Minor structural changes in the directory

--- back/out/routes/Spotify.js
+++ back/out/routes/Spotify.js
@@ -1,12 +1,7 @@
 import express from "express";
-
 const spotifyRouter = express.Router();
-
-
 //example
 /*
 spotifyRouter.get( request stuff )
 */
-
-
 export default spotifyRouter;
--- back/out/src/server.js
+++ back/out/src/server.js
@@ -1,11 +1,8 @@
-import express, { Response } from "express";
-import sqlite3 from "sqlite3";
-import { open } from "sqlite";
+import express from "express";
 import cookieParser from "cookie-parser";
-import bandsInTownRouter from "./routes/bandsInTown";
-import spotifyRouter from "./routes/Spotify";
-import ticketMasterRouter from "./routes/ticketMaster";
-
+import bandsInTownRouter from "../routes/bandsInTown";
+import spotifyRouter from "../routes/Spotify";
+import ticketMasterRouter from "../routes/ticketMaster";
 let app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -13,11 +10,10 @@ app.use("/bands", bandsInTownRouter);
 app.use("/spotify", spotifyRouter);
 app.use("/ticketmaster", ticketMasterRouter);
 app.use(express.static("public"));
-
 // run server
 let port = 3000;
 let host = "localhost";
 let protocol = "http";
 app.listen(port, host, () => {
-  console.log(`${protocol}://${host}:${port}`);
+    console.log(`${protocol}://${host}:${port}`);
 });
