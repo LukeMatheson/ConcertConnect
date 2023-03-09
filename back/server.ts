4eb63fd16b126f9819ef6cb1881521b1ff490010
Nizom Djuraev <nd596@drexel.edu>
2023-03-08 22:41:20 -0500

    Fixed npm start
    Server starts normally now, didn't realize last commit for this was faulty.

--- back/server.ts
+++ back/server.ts
@@ -1,8 +1,11 @@
-import express from "express";
+import express, { Response } from "express";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
 import cookieParser from "cookie-parser";
-import bandsInTownRouter from "../routes/bandsInTown";
-import spotifyRouter from "../routes/Spotify";
-import ticketMasterRouter from "../routes/ticketMaster";
+import bandsInTownRouter from "./routes/bandsInTown.js";
+import spotifyRouter from "./routes/Spotify.js";
+import ticketMasterRouter from "./routes/ticketMaster.js";
+
 let app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -10,10 +13,11 @@ app.use("/bands", bandsInTownRouter);
 app.use("/spotify", spotifyRouter);
 app.use("/ticketmaster", ticketMasterRouter);
 app.use(express.static("public"));
+console.log("o");
 // run server
 let port = 3000;
 let host = "localhost";
 let protocol = "http";
 app.listen(port, host, () => {
-    console.log(`${protocol}://${host}:${port}`);
+  console.log(`${protocol}://${host}:${port}`);
 });
