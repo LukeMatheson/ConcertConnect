8beceefc8d2eacca9fdc3d08a1da15dafc5bb100
Luke Matheson <38731081+cardinal7987@users.noreply.github.com>
2023-03-10 17:23:26 -0500

    final pushes before merge

--- back/out/routes/Spotify.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-const spotifyRouter = express.Router();
-//example
-/*
-spotifyRouter.get( request stuff )
-*/
-export default spotifyRouter;
--- back/src/server.ts
+++
@@ -1,1 +0,0 @@
-//r emove this file.
