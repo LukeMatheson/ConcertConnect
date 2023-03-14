5d39f1bf6270f617cfd9f269276c0adb6dca43ec
Nizom Djuraev <nd596@drexel.edu>
2023-03-14 15:32:10 -0400

    Implemented EventSearch and simple navbar for testing
    Implemented event search by artist name that returns the venue, date, lineup, and location along with an image of the artist.

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Login from './login';
+import App from './App';
+
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -8,6 +10,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <Login />
+    <App />
   </React.StrictMode>
 );
