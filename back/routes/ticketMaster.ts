36f5d1ac418c5f97aa2bbb289a3741296b23c9a3
Wyatt Kaiser <wyatt.kaiser@outlook.com>
2023-03-15 16:14:25 -0400

    ticketmaster working. gets event link

--- back/routes/ticketMaster.ts
+++ back/routes/ticketMaster.ts
@@ -7,20 +7,17 @@ const apiKey = env.ticketMaster;
 // get an artist by keyword and city
 ticketMasterRouter.get("/eventTickets", async (req, res) => {
   try {
-    // const artistName = req.query.artistName;
-    // const eventCity = req.query.eventCity;
+    const artistName = req.query.artist;
+    const eventCity = req.query.city;
 
-    let artistName = "Drake";
-    let eventCity = "Philadelphia";
-    // let url = `http://app.ticketmaster.com/discovery/v2/events.json?keyword=${artistName}&city=${eventCity}&apikey=${apiKey}`;
-    let url = `http://app.ticketmaster.com/discovery/v2/events.json?keyword=Drake&city=Philadelphia&apikey=${apiKey}`;
+    let url = `http://app.ticketmaster.com/discovery/v2/events.json?keyword=${artistName}&city=${eventCity}&apikey=${apiKey}`;
     let response = await fetch(url);
     let data = await response.json();
 
-    // get data from ticketmaster
+    // get event link from ticketmaster
     let eventUrl = data._embedded.events[0].url;
-    let artistUrl = data._embedded.venues._embedded.dmas[0].url;
-    res.json({ data: data });
+
+    res.json({ link: eventUrl });
   } catch (error) {
     res.status(500).json({ message: "Server Error" });
   }
