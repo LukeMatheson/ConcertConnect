f79b7901911c2ae815e9dc848edcb3821e7d8dcc
Nizom Djuraev <nd596@drexel.edu>
2023-03-16 12:19:37 -0400

    Implemented notification via email

--- back/routes/twilio.ts
+++ back/routes/twilio.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import env from "../../env.json" assert { type: "json" };
 import twilio from "twilio";
+import sendgrid from '@sendgrid/mail';
 
 let twilioRoute = express.Router();
 
 let accountSid = env.twilioAccountSID;
 let authToken = env.twilioAuthToken;
+let twilioSendgridKey = env.twilioSendgridKey;
+sendgrid.setApiKey(twilioSendgridKey);
 
 let client = twilio(accountSid, authToken);
 
@@ -16,8 +19,14 @@ twilioRoute.post('/sendMessage', async (req, res) => {
         let messageBody = `A friend has invited to this concert starring Lineup: ${lineup}. This event will be at ${name} on ${date} in ${location}`;
         let message;
         if (notificationMethod === "email") {
-            // TODO: send email
-            message = "Email sent!";
+            const msg: sendgrid.MailDataRequired = {
+                to: contactInfo,
+                from: 'nd596@drexel.edu',
+                subject: `Invitation to ${lineup} concert`,
+                text: messageBody,
+                html: `<p>${messageBody}</p>`,
+            };
+            message = await sendgrid.send(msg);
         } else if (notificationMethod === "sms") {
             message = await client.messages.create({
                 body: messageBody,
