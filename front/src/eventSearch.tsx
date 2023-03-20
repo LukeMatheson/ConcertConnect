8bafd87d91140b70faad673423135bfee3ae7563
Wyatt Kaiser <wyatt.kaiser@outlook.com>
2023-03-20 01:16:13 -0400

    search page styled

--- front/src/eventSearch.tsx
+++ front/src/eventSearch.tsx
@@ -1,133 +1,195 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { style } from "@mui/system";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  Button,
+  Box,
+  Typography,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  SelectChangeEvent,
+} from "@mui/material";
 
-let spotifyID = sessionStorage.getItem('spotifyID');
+let spotifyID = sessionStorage.getItem("spotifyID");
 
 interface Event {
-    artist: {
-        image_url: string;
-    };
-    venue: {
-        name: string;
-        location: string;
-        latitude: number;
-        longitude: number;
-    };
-    datetime: string;
-    lineup: string[];
-    artistName: string;
+  artist: {
+    image_url: string;
+  };
+  venue: {
+    name: string;
+    location: string;
+    latitude: number;
+    longitude: number;
+  };
+  datetime: string;
+  lineup: string[];
+  artistName: string;
 }
 
-
 let EventSearch: React.FC = () => {
-    let [searchTerm, setSearchTerm] = useState<string>('');
-    let [eventData, setEventData] = useState<Event[]>([]);
-    let [searchResults, setMessage] = useState(false);
+  let [searchTerm, setSearchTerm] = useState<string>("");
+  let [eventData, setEventData] = useState<Event[]>([]);
+  let [searchResults, setMessage] = useState(false);
 
-    let navigate = useNavigate();
+  let navigate = useNavigate();
 
+  let handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
 
-    let handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setSearchTerm(event.target.value);
-    };
+  let handleSearchSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      let response = await fetch(`/bands/artistEvents?artist=${searchTerm}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      console.log(response);
+      if (response.status == 200) {
+        let data: Event[] = await response.json();
+        let eventDataWithArtistName = data.map((event) => ({
+          ...event,
+          artistName: searchTerm,
+          datetime: new Date(event.datetime).toLocaleString(),
+        }));
 
-    let handleSearchSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        try {
-            let response = await fetch(`/bands/artistEvents?artist=${searchTerm}`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
-            console.log(response);
-            if (response.status == 200) {
-                let data: Event[] = await response.json();
-                let eventDataWithArtistName = data.map((event) => ({
-                    ...event,
-                    artistName: searchTerm,
-                    datetime: new Date(event.datetime).toLocaleString()
-                }));
+        setEventData(eventDataWithArtistName);
+        setMessage(false);
+      } else {
+        setMessage(true);
+      }
+    } catch (error) {
+      setMessage(true);
+      console.error(error);
+    }
+  };
 
-                setEventData(eventDataWithArtistName);
-                setMessage(false);
-            } else {
-                setMessage(true);
-            }
+  let handleViewEvent = (eventIndex: number) => {
+    navigate("/viewEvent", { state: eventData[eventIndex] });
+  };
 
-        } catch (error) {
-            setMessage(true);
-            console.error(error);
-        }
-    };
+  let handleSaveEvent = async (eventIndex: number) => {
+    try {
+      console.log("saving");
+      let eventFields = {
+        //Using my own spotify ID for not until I can grab spotify ID from cookie or however Luke makes it available
+        spotifyID: spotifyID,
+        artistName: eventData[eventIndex].artistName,
+        venue: eventData[eventIndex].venue.name,
+        dateTime: eventData[eventIndex].datetime,
+        lineup: JSON.stringify(eventData[eventIndex].lineup),
+        location: eventData[eventIndex].venue.location,
+        latitude: eventData[eventIndex].venue.latitude,
+        longitude: eventData[eventIndex].venue.longitude,
+      };
 
-    let handleViewEvent = (eventIndex: number) => {
-        navigate('/viewEvent', { state: eventData[eventIndex] });
-    };
+      let response = await fetch("/bands/saveEvent", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(eventFields),
+      });
 
-    let handleSaveEvent = async (eventIndex: number) => {
-        try {
-            console.log("saving");
-            let eventFields = {
-                //Using my own spotify ID for not until I can grab spotify ID from cookie or however Luke makes it available
-                spotifyID: spotifyID,
-                artistName: eventData[eventIndex].artistName,
-                venue: eventData[eventIndex].venue.name,
-                dateTime: eventData[eventIndex].datetime,
-                lineup: JSON.stringify(eventData[eventIndex].lineup),
-                location: eventData[eventIndex].venue.location,
-                latitude: eventData[eventIndex].venue.latitude,
-                longitude: eventData[eventIndex].venue.longitude
-            };
+      if (response.status === 200) {
+        console.log("Event saved successfully!");
+      } else {
+        console.error("Error saving event");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-            let response = await fetch('/bands/saveEvent', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(eventFields)
-            });
-
-            if (response.status === 200) {
-                console.log('Event saved successfully!');
-            } else {
-                console.error('Error saving event');
-
-            }
-
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    return (
-        <div className="content">
-            <h1>Search Upcoming Events</h1>
-            <form onSubmit={handleSearchSubmit}>
-                <label htmlFor="artist-name">Artist Name:</label>
-                <input type="text" name="artist-name" value={searchTerm} onChange={handleSearch} />
-                <button type="submit">Search</button>
-            </form>
-            {searchResults && (
-                <p style={{ marginTop: "10px", color: "maroon" }}>Not found</p>
-            )}
-            {eventData && eventData.length > 0 && (
-                <>
-                    {eventData[0].artist && eventData[0].artist.image_url && <img src={eventData[0].artist.image_url} alt="Event" style={{ width: "300px" }} />}
-                    {eventData.map((event, index) => (
-                        <div key={index} style={{ border: "1px solid black", padding: "10px", marginBottom: "10px" }}>
-                            <p><strong>Venue: </strong>{event.venue.name}</p>
-                            <p><strong>Date: </strong>{event.datetime}</p>
-                            <p><strong>Lineup: </strong>{event.lineup.join(', ')}</p>
-                            <p><strong>Location: </strong>{event.venue.location}</p>
-                            <button onClick={() => handleViewEvent(index)}>View Event</button>
-                        </div>
-                    ))}
-                </>
-            )}
-        </div>
-    );
-
+  return (
+    <Box className="content">
+      <Typography variant="h4" gutterBottom sx={{ marginLeft: "10px" }}>
+        Search Upcoming Events
+      </Typography>
+      <form onSubmit={handleSearchSubmit}>
+        <FormControl sx={{ marginLeft: "10px" }}>
+          <TextField
+            id="artist-name"
+            label="Artist Name"
+            variant="outlined"
+            value={searchTerm}
+            onChange={handleSearch}
+          />
+        </FormControl>
+        <FormControl sx={{ marginTop: "10px" }}>
+          <Button
+            variant="contained"
+            type="submit"
+            sx={{ backgroundColor: "green" }}
+          >
+            Search
+          </Button>
+        </FormControl>
+      </form>
+      {searchResults && (
+        <Typography variant="body1" sx={{ marginTop: "10px", color: "maroon" }}>
+          Not found
+        </Typography>
+      )}
+      {eventData && eventData.length > 0 && (
+        <>
+          {eventData[0].artist && eventData[0].artist.image_url && (
+            <img
+              src={eventData[0].artist.image_url}
+              alt="Event"
+              style={{
+                width: "300px",
+                display: "block",
+                marginLeft: "auto",
+                marginRight: "auto",
+                paddingBottom: "1em",
+              }}
+            />
+          )}
+          {eventData.map((event, index) => (
+            <Box
+              key={index}
+              sx={{
+                border: "1px solid black",
+                padding: "10px",
+                marginBottom: "10px",
+              }}
+            >
+              <Typography variant="body1">
+                <strong>Venue: </strong>
+                {event.venue.name}
+              </Typography>
+              <Typography variant="body1">
+                <strong>Date: </strong>
+                {event.datetime}
+              </Typography>
+              <Typography variant="body1">
+                <strong>Lineup: </strong>
+                {event.lineup.join(", ")}
+              </Typography>
+              <Typography variant="body1">
+                <strong>Location: </strong>
+                {event.venue.location}
+              </Typography>
+              <Button
+                onClick={() => handleViewEvent(index)}
+                variant="contained"
+                sx={{ backgroundColor: "green" }}
+              >
+                View Event
+              </Button>
+            </Box>
+          ))}
+        </>
+      )}
+    </Box>
+  );
 };
 
 export default EventSearch;
